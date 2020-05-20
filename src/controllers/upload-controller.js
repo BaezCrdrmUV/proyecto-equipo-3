c2f1ce1fb9be783d0b23136a7943953f45c3579f
davisbd100 <davisbd100@gmail.com>
2020-05-20 16:07:47 -0500

    Creacion de directorios

--- src/controllers/upload-controller.js
+++ src/controllers/upload-controller.js
@@ -1,24 +1,57 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+const ffmpeg = require('fluent-ffmpeg')
 var fs = require('fs');
 
 
 const uploadsong = async(req, res) =>{
-    var nombre = req.file.originalname;
-    var archivoCanciones = path.join(songDir, 'songs.json');
-    fs.readFile(archivoCanciones, 'utf8', function(err, archivo) {
-        if (err) throw err;
-        var songs = JSON.parse(archivo);
-        songs.push({
-            nombre: nombre,
-            nombrearchivo: nombre
-        });
-        fs.writeFile(archivoCanciones, JSON.stringify(songs), function(err) {
-            if (err) throw err;
-            res.send({status: 'ok' , message: 'Cancion agregada'})
-        });
-    });
+   console.log('Cancion recibida: ' + req.file.originalname);
+   res.json({message: 'Cancion recibida: ' + req.file.originalname});
+   resizeDiferentAudio(req.file.originalname);
 };
 
+
 module.exports = {uploadsong};
+
+function resizeDiferentAudio(song){
+    var songOnlyName = path.basename(song, '.mp3');
+    makeDirectory(songDir + '/' + songOnlyName);
+    resizeAudio(songOnlyName, 128);
+    resizeAudio(songOnlyName, 160);
+    resizeAudio(songOnlyName, 192);
+    resizeAudio(songOnlyName, 320);
+}
+
+function makeDirectory(directory){
+    if (!fs.existsSync(directory)){
+        fs.mkdirSync(directory);
+        console.log('Directory Created');
+    }else{
+        console.log('Directory already exists')
+    }
+}
+
+function resizeAudio(song, bitrate) {
+    const name = song;
+    makeDirectory(`${songDir}/${name}/${bitrate}`);
+    const command = ffmpeg(`${songDir}/${song}.mp3`)
+    .audioCodec('libmp3lame')
+    .audioBitrate(bitrate)
+    .outputOptions([
+        '-hls_time 10',
+        '-segment_time 10',
+        '-segment_format mpegts',
+        '-hls_playlist_type vod',
+        '-hls_segment_filename ' + `${songDir}/${name}//${bitrate}/${bitrate}-%03d.ts`,
+        '-segment_list ' + `${songDir}/${name}/${bitrate}/${bitrate}.m3u8`
+    ])
+    .output(`${songDir}/${name}/${bitrate}/${bitrate}.m3u8`)
+    .on('progress', function(progress) {
+        console.log('Processing: ' + progress.percent + '% done')
+    })
+    .on('end', function(err, stdout, stderr) {
+        console.log('Finished processing!' /*, err, stdout, stderr*/)
+    })
+    .run() 
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes/upload-routes');
-global.songDir = path.resolve('/home/davisbd/Escritorio/proyecto-equipo-3/songs');
+global.songDir = path.resolve('/home/davisbd/Escritorio/Songs');
 
 
 app.use(express.json());
