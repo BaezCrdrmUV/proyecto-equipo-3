3187806354fc6b40a69cb3d7242fdfacfbff84e5
jarturo <jarturogl05@gmail.com>
2020-06-05 11:49:38 -0500

    Creacion de metodo para recuperar metadatos

--- metadata-service/src/routes/song-routes.js
+++ metadata-service/src/routes/song-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const songController = require('../controllers/song-controller.js');
 
 router.post('/songs/createSong', songController.createSong);
-
+router.get('/songs/getSong', songController.getSong);
 
 
 module.exports = router;
