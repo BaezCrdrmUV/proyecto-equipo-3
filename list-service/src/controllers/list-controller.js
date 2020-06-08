525fd4be2deaf5e1be49df5b4cc236d1d5aea568
jarturo <jarturogl05@gmail.com>
2020-06-08 12:51:51 -0500

    estructura del proyecto

---
+++ list-service/src/controllers/list-controller.js
@@ -0,0 +1,23 @@
+const List = require('../mongo/models/list.js');
+
+const createList = async (req, res) =>{
+
+};
+
+const addSongList = async (req, res) =>{
+
+};
+
+const removeSongList = async (req, res) =>{
+
+};
+
+const deleteList = async (req, res) =>{
+
+};
+
+const getLists = async (req, res) =>{
+
+};
+
+module.exports = {createList, addSongList, removeSongList, deleteList, getLists};
---
+++ list-service/src/routes/list-routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const playlistController = require('../controllers/list-controller.js');
+
+router.post('/playlist/CreatePlaylist', playlistController.createList);
+router.put('/playlist/AddSong', playlistController.addSongList);
+router.put('/playlist/RemoveSong', playlistController.removeSongList);
+router.delete('/playlist/Deletelist', playlistController.deleteList);
+router.get('/playlist/GetPlaylist', playlistController.getLists);
+
+module.exports = router;
