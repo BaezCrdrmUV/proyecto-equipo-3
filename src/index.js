c1424713af0154a5271bbd51b4c9b014978afa53
davisbd100 <davisbd100@gmail.com>
2020-06-11 13:04:18 -0500

    Correccion de entrega 'CORS'

--- src/index.js
+++ src/index.js
@@ -4,23 +4,36 @@ const path = require('path');
 global.songDir = path.resolve('../songs');
 const routes = require('./routes/streaming-routes.js')
 
-app.use(express.json());
-app.use(routes);
+// app.use(express.json());
+// app.use(routes);
 
-const PORT = 3000;
+// const PORT = 3000;
 
-var mongoose = require("mongoose");
-mongoose.set('useFindAndModify', false);
-mongoose
-.connect("mongodb://localhost:27017/songs", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-    console.log("conexión creada");
-    app.listen(PORT, function(){
-      console.log("server running");
-  });
-}).catch(Error => {
-    console.log("mongo error", Error);
+// var mongoose = require("mongoose");
+// mongoose.set('useFindAndModify', false);
+// mongoose
+// .connect("mongodb://localhost:27017/songs", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//     console.log("conexión creada");
+//     app.listen(PORT, function(){
+//       console.log("server running");
+//   });
+// }).catch(Error => {
+//     console.log("mongo error", Error);
+// });
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
+
+app.use(express.static(path.join(songDir)));
+
+app.listen(3000);
+console.log('Listening on Port 8000');
