5216bc7d0e10bacd6b90945a4d5e80dd6b2d3c4d
davisbd100 <davisbd100@gmail.com>
2020-06-12 13:37:28 -0500

    Docker compose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-global.songDir = path.resolve('../songs');
+global.songDir = path.resolve('./songs');
 
 const PORT = 3000;
 
