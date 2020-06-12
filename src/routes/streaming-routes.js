ee0915c2a2e9bd437d8c2b6511aaf0fc7871af84
davisbd100 <davisbd100@gmail.com>
2020-06-12 13:31:58 -0500

    Eliminacion de clases inecesarias

--- src/routes/streaming-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router(); 
-const streamingController = require('../controllers/streaming-controller')
-
-
-router.get('/getsong', streamingController.getSong);
-
-module.exports = router;
