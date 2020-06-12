814d6c2630b82d0e6e82d1ea1ec3a1824c648192
davisbd100 <davisbd100@gmail.com>
2020-06-12 13:30:31 -0500

    Eliminación de codigo inecesario

--- src/controllers/streaming-controller.js
+++ src/controllers/streaming-controller.js
@@ -1,11 +1,21 @@
 
 var express = require('express');
 var app = express();
+const path = require('path');
+const fs = require('fs')
 
 
 const getSong = async(req, res) =>{
-   console.log('Cancion pedida: ' + req.body);
-   res.send(req.body);
+   var file = path.join(songDir, 'GimmeLove', 'GimmeLove' + '.m3u8');
+   console.log(file);
+   res.writeHead(200, {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Headers': 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE',
+      'Allow': 'GET, POST, OPTIONS, PUT, DELETE',
+      'Content-Type': 'application/vnd.apple.mpegurl'
+    });
+    fs.createReadStream(file).pipe(res);
 };
 
 
--- src/index.js
+++ src/index.js
@@ -2,28 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 global.songDir = path.resolve('../songs');
-const routes = require('./routes/streaming-routes.js')
 
-// app.use(express.json());
-// app.use(routes);
-
-// const PORT = 3000;
-
-// var mongoose = require("mongoose");
-// mongoose.set('useFindAndModify', false);
-// mongoose
-// .connect("mongodb://localhost:27017/songs", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => {
-//     console.log("conexión creada");
-//     app.listen(PORT, function(){
-//       console.log("server running");
-//   });
-// }).catch(Error => {
-//     console.log("mongo error", Error);
-// });
+const PORT = 3000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -32,8 +12,8 @@ app.use((req, res, next) => {
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+app.use(express.static(songDir));
 
-app.use(express.static(path.join(songDir)));
-
-app.listen(3000);
-console.log('Listening on Port 8000');
+app.listen(PORT, function(){
+  console.log('Listening on port: ' + PORT);
+});
