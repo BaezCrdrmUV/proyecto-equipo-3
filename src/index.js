1d2474e643dbbc7ae8a56e21455674193c8acc2d
davisbd100 <davisbd100@gmail.com>
2020-06-24 17:00:34 -0500

    Correccion de punto y coma

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
 global.songDir = path.resolve('../songs');
 
 const PORT = 3000;
 
-app.use(cors)
+app.use(cors);
 app.use(express.static(songDir));
 
 app.listen(PORT, function(){
