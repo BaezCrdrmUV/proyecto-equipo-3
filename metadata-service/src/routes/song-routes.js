3d5eae8a0b8ee6375a2de3552250e1ea9bcb983b
davisbd100 <davisbd100@gmail.com>
2020-07-01 13:29:03 -0500

    Rutas de artistas y albumes

--- metadata-service/src/routes/song-routes.js
+++ metadata-service/src/routes/song-routes.js
@@ -5,5 +5,11 @@ const songController = require('../controllers/song-controller.js');
 router.post('/songs/createSong', songController.createSong);
 router.get('/songs/getSong', songController.getSong);
 
+router.post('/songs/createArtist', songController.createArtist);
+router.get('/songs/getArtist', songController.getArtist);
+
+router.post('/songs/createAlbum', songController.createAlbum);
+router.get('/songs/getAlbum', songController.getAlbum);
+
 
 module.exports = router;
