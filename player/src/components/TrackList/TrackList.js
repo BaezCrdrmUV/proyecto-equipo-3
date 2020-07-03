801b49478b03d28de5a8fd6dbda8db76e03ac726
jarturo <jarturogl05@gmail.com>
2020-07-03 18:27:17 -0500

    song options

--- player/src/components/TrackList/TrackList.js
+++ player/src/components/TrackList/TrackList.js
@@ -1,132 +1,148 @@
-import React, { Component } from 'react';
-import './TrackList.css';
-import {connect} from 'react-redux';
-import {getSelectedPlaylist} from'../../redux/actions/playlists';
-import {getSongs} from '../../redux/actions/songs';
-import {currentSong} from '../../redux/actions/songs';
+import React, { Component } from "react";
+import "./TrackList.css";
+import { connect } from "react-redux";
+import { getSelectedPlaylist } from "../../redux/actions/playlists";
+import { getSongs } from "../../redux/actions/songs";
+import { currentSong } from "../../redux/actions/songs";
+import { Dropdown } from "react-bootstrap";
 
 
- class TrackList extends Component {
+class TrackList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSongs: "",
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          selectedSongs: ""
-          
-        }
+  componentDidMount() {
+    this.props.getSelectedPlaylist(this.props.elementToRender.id);
+    const selectedPlaylist = this.props.playlists.selectedPlaylist;
+    const songsInPlaylist = selectedPlaylist.songs;
+    this.props.getSongs(songsInPlaylist);
+    console.log(songsInPlaylist);
+    console.log(this.props);
+    this.setState({ selectedSongs: this.props.elementToRender.id });
+  }
+
+  componentDidUpdate() {
+    if (this.state.selectedSongs !== this.props.elementToRender.id) {
+      this.setState({ selectedSongs: this.props.elementToRender.id });
+      this.props.getSelectedPlaylist(this.props.elementToRender.id);
+      const selectedPlaylist = this.props.playlists.selectedPlaylist;
+      const songsInPlaylist = selectedPlaylist.songs;
+      this.props.getSongs(songsInPlaylist);
+      console.log(songsInPlaylist);
+      console.log(this.props);
     }
+  }
 
-    componentDidMount(){
-            this.props.getSelectedPlaylist(this.props.elementToRender.id);
-            const selectedPlaylist = this.props.playlists.selectedPlaylist;
-            const songsInPlaylist = selectedPlaylist.songs; 
-            this.props.getSongs(songsInPlaylist);
-            console.log(songsInPlaylist);
-            console.log(this.props);
-            this.setState({selectedSongs: this.props.elementToRender.id});
+  playSong(song) {
+    this.props.currentSong(song);
+  }
 
-    }
 
-    componentDidUpdate(){
-        if(this.state.selectedSongs !== this.props.elementToRender.id){
-            this.setState({selectedSongs: this.props.elementToRender.id});
-            this.props.getSelectedPlaylist(this.props.elementToRender.id);
-            const selectedPlaylist = this.props.playlists.selectedPlaylist;
-            const songsInPlaylist = selectedPlaylist.songs; 
-            this.props.getSongs(songsInPlaylist);
-            console.log(songsInPlaylist);
-            console.log(this.props);
-        }
+  renderSongOptions(){
+    const playlists = this.props.playlists.playlists;
+    return (     
+        <div>
+            {
+                playlists.map(playlist => {
+                    return <Dropdown.Item key={playlist.id}>
+                        {playlist.name}
+                      </Dropdown.Item>   
+                })
+            }
+        </div>
+    )
+  }
 
-    }
+  renderSongs() {
+    return (
+      <div>
+        {this.props.songs.listSongs.map((song) => {
+          return (
+            <div className="user-song-item" key={song.id}>
+              <button
+                className="play-button"
+                onClick={() => this.playSong(song)}
+              ></button>
+              <div className="song-title">
+                <p>{song.title}</p>
+              </div>
 
-    playSong(song){
-        this.props.currentSong(song);
+              <div className="song-artist">
+                <p>{song.artist}</p>
+              </div>
 
-    }
+              <div className="song-album">
+                <p>{song.album}</p>
+              </div>
 
-    renderSongs() {
-        return (
-            <div>
-                {
-                    this.props.songs.listSongs.map(song => {
-                        return <div className="user-song-item" key={song.id}>
-                            <button className="play-button" onClick={() => this.playSong(song)}>
+              <div className="song-length">
+                <p>{song.duration}</p>
+              </div>
 
-                            </button>                           
-                            <div className="song-title">
-                                <p>{song.title}</p>
-                            </div>
+              <div className="optionButton">
+                <Dropdown>
 
-                            <div className="song-artist">
-                                <p>{song.artist}</p>
-                            </div>
+                  <Dropdown.Toggle  split variant="light" id="dropdown-split-basic" />
 
-                            <div className="song-album">
-                                <p>{song.album}</p>
-                            </div>
 
-                            <div className="song-length">
-                                <p>{song.duration}</p>
-                            </div>
+                  <Dropdown.Menu>
+                  <Dropdown.Item>Add to Queue</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Header>Add to playlist:</Dropdown.Header>
+                    {this.renderSongOptions()}
+                  </Dropdown.Menu>
 
-                        </div>
-                    })
-                }
+                </Dropdown>
+              </div>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            
+  render() {
+    return (
+      <div>
+        <div className="section-title">Playlist</div>
+        <div className="song-header-container">
+          <div className="song-title-header">
+            <p>Title</p>
+          </div>
+          <div className="song-artist-header">
+            <p>Artist</p>
+          </div>
+          <div className="song-album-header">
+            <p>Album</p>
+          </div>
+          <div className="song-length-header">
+            <p>Duration</p>
+          </div>
+        </div>
 
-            <div>
-                <div className="section-title">Playlist</div>
-                <div className="song-header-container">
-                    <div className="song-title-header">
-                        <p>Title</p>
-                    </div>
-                    <div className="song-artist-header">
-                        <p>Artist</p>
-                    </div>
-                    <div className="song-album-header">
-                        <p>Album</p>
-                    </div>
-                    <div className="song-length-header">
-                        <p>Duration</p>
-                    </div>
-                </div>
-
-                {this.renderSongs()}
-            </div>
-
-        )
-    }
+        {this.renderSongs()}
+      </div>
+    );
+  }
 }
-
 
 const mapStateToProps = (state) => {
-    return {
-        elementToRender : state.elementToRender,
-        playlists : state.playlists,
-        songs : state.songs
-        
-        
-    };
+  return {
+    elementToRender: state.elementToRender,
+    playlists: state.playlists,
+    songs: state.songs,
+
+  };
 };
 
+const mapDispatchToProps = {
+  getSelectedPlaylist,
+  getSongs,
+  currentSong,
+};
 
-
-const mapDispatchToProps = { 
-    
-    getSelectedPlaylist,
-    getSongs,
-    currentSong
-    
-
-}
-
-
-
-export default connect (mapStateToProps, mapDispatchToProps) (TrackList);
+export default connect(mapStateToProps, mapDispatchToProps)(TrackList);
