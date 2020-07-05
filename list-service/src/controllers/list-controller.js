4234c73edeca0b8eda1f059a33094370c9c07f34
davisbd100 <davisbd100@gmail.com>
2020-07-04 23:29:21 -0500

    Agregar canciones

--- list-service/src/controllers/list-controller.js
+++ list-service/src/controllers/list-controller.js
@@ -1,7 +1,6 @@
 const List = require('../mongo/models/list.js');
 
 const createList = async (req, res) =>{
-
     try {
         const { user, name } = req.body;
         if (List.exists({ user: user, name: name })) {
@@ -26,7 +25,23 @@ const createList = async (req, res) =>{
 };
 
 const addSongList = async (req, res) =>{
-
+    try {
+        const { listId, songs } = req.body;
+        if (List.findById(listId) == null){
+            res.status(404).send({ status: 'ERROR', message: 'Lista no encontrada' });
+        }else{
+            List.findOneAndUpdate({_id: listId},
+                {$push: {songs: songs}, function (error, response) {
+                    if (error){
+                        res.status(403).send({ status: 'ERROR', message: 'Error al agregar las canciones' });
+                    }else{
+                        res.status(200).send({ status: 'ok', message: 'Canciones agregadas' });
+                    }
+                }});
+        }
+    } catch (error) {
+        res.status(404).send({ status: 'ERROR', message: 'Lista no encontrada' });
+    }
 };
 
 const removeSongList = async (req, res) =>{
@@ -38,7 +53,18 @@ const deleteList = async (req, res) =>{
 };
 
 const getLists = async (req, res) =>{
+    try {
+        const { listId } = req.body;
+        const list = await List.findById(listId);
+        if (list == null){
+            res.status(404).send({ status: 'ERROR', message: 'Lista no encontrada' });
+        }else{
+            res.send({ status: 'ok', data: song });
+        }
 
+    } catch (error) {
+        res.status(404).send({ status: 'ERROR', message: 'Lista no encontrada' });
+    }
 };
 
 module.exports = {createList, addSongList, removeSongList, deleteList, getLists};
