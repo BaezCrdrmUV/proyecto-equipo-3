b99a58437050c29625032c4d83ec4a41f740ec03
davisbd100 <davisbd100@gmail.com>
2020-07-05 14:15:00 -0500

    Carpeta de imagenes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 global.songDir = path.resolve('../songs');
+global.songDir = path.resolve('../images');
 
 const PORT = 3000;
 
@@ -12,6 +13,7 @@ app.use((req, res, next) => {
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');	
   next();	
 });
+app.use(express.static(songDir));
 app.use(express.static(songDir));
 
 
