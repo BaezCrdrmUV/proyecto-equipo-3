07d557ce80cd0e1275a9044c6165d506809929f8
davisbd100 <davisbd100@gmail.com>
2020-07-06 20:20:05 -0500

    Cambios a rutas http

--- list-service/src/routes/list-routes.js
+++ list-service/src/routes/list-routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const playlistController = require('../controllers/list-controller.js');
 
 router.post('/playlist/CreatePlaylist', playlistController.createList);
-router.put('/playlist/AddSong', playlistController.addSongList);
+router.post('/playlist/AddSong', playlistController.addSongList);
 router.put('/playlist/RemoveSong', playlistController.removeSongList);
 router.delete('/playlist/Deletelist', playlistController.deleteList);
-router.get('/playlist/GetPlaylist', playlistController.getLists);
-router.get('/playlist/GetMyPlaylist', playlistController.getMyPlaylists);
+router.post('/playlist/GetPlaylist', playlistController.getLists);
+router.post('/playlist/GetMyPlaylist', playlistController.getMyPlaylists);
 
 module.exports = router;
