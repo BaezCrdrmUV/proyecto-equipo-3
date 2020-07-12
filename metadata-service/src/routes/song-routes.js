32743e2f6670b91b76adb86f4754da47bb6b30ea
davisbd100 <davisbd100@gmail.com>
2020-07-12 15:51:10 -0500

    Cambios a tipos de request de routes

--- metadata-service/src/routes/song-routes.js
+++ metadata-service/src/routes/song-routes.js
@@ -5,14 +5,14 @@ const songController = require('../controllers/song-controller.js');
 router.post('/songs/createSong', songController.createSong);
 router.post('/songs/createSongs', songController.createSongs);
 router.post('/songs/getSongId', songController.getSong);
-router.get('/songs/getSongByName', songController.getSongByName);
-router.get('/songs/getSongByGenre', songController.getSongByGenre);
+router.post('/songs/getSongByName', songController.getSongByName);
+router.post('/songs/getSongByGenre', songController.getSongByGenre);
 
 router.post('/songs/createArtist', songController.createArtist);
-router.get('/songs/getArtist', songController.getArtist);
+router.post('/songs/getArtist', songController.getArtist);
 
 router.post('/songs/createAlbum', songController.createAlbum);
-router.get('/songs/getAlbum', songController.getAlbum);
+router.post('/songs/getAlbum', songController.getAlbum);
 
 router.post('/songs/getSongsByAlbum', songController.getSongsByAlbum)
 router.get('/songs/getAllAlbums', songController.getAllAlbums);
